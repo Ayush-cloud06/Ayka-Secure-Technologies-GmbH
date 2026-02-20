17836cf75a9d3c6fac14e63200aaa24fd7058d1c
Ayush Yadav <official.ayush.066@gmail.com>
2026-02-20 16:08:45 +0530

    Organizational units

---
+++ Internal-IT/cloud-platform/ou-structure/outputs.tf
@@ -0,0 +1,11 @@
+output "Security_ou_id" {
+  value = aws_organizations_organizational_unit.Security.id
+}
+
+output "Infrastructure_ou_id" {
+  value = aws_organizations_organizational_unit.Infrastructure.id
+}
+
+output "Workloads" {
+  value = aws_organizations_organizational_unit.Workloads.id
+}
