94a9fa3557f47fc8885b7beda9fec693647d258d
Ayush Yadav <official.ayush.066@gmail.com>
2026-02-28 01:32:06 +0530

    break glass admin

--- Internal-IT/iam/entra-id/break_glass.tf
+++ Internal-IT/iam/entra-id/break_glass.tf
@@ -1,1 +1,14 @@
+resource "azuread_user" "break_glass_1" {
+  user_principal_name = "breakglass-01@ayushgta175outlook.onmicrosoft.com"
+  display_name        = "Break Glass Emergency 01"
+  mail_nickname       = "breakglass01"
 
+  password                    = "SuperStrongTempPassword!"
+  force_password_change       = false
+  disable_password_expiration = true
+}
+
+resource "azuread_group_member" "break_glass_tier0" {
+  group_object_id  = azuread_group.tier_groups["tier0"].id
+  member_object_id = azuread_user.break_glass_1.id
+}
