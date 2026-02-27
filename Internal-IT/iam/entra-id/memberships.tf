6d13eaf6f502af07f73bb4f044dad135eb76979a
Ayush Yadav <official.ayush.066@gmail.com>
2026-02-28 01:18:43 +0530

    EntraId Memberships

---
+++ Internal-IT/iam/entra-id/memberships.tf
@@ -0,0 +1,27 @@
+# ----- Department Membersips -----
+resource "azuread_group_member" "department_memberships" {
+
+  for_each = local.normalized_personnel
+
+  group_object_id  = azuread_group.department_groups[each.value.department_key].id
+  member_object_id = azuread_user.users[each.key].id
+}
+
+# ------ Security ROle memberships ------
+resource "azuread_group_member" "security_role_memberships" {
+
+  for_each = local.normalized_personnel
+
+  group_object_id  = azuread_group.security_role_groups[each.value.security_role_key].id
+  member_object_id = azuread_user.users[each.key].id
+}
+
+
+# ------ Tier Membersips ------
+resource "azuread_group_member" "tier_memberships" {
+
+  for_each = local.normalized_personnel
+
+  group_object_id  = azuread_group.tier_groups[each.value.tier_key].id
+  member_object_id = azuread_user.users[each.key].id
+}
