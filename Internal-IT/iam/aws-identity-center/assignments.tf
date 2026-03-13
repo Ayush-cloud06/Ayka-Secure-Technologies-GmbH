ce2af175e7812f09ca2f093f0771ebcaabed3fe7
Ayush Yadav <official.ayush.066@gmail.com>
2026-03-13 21:51:32 +0530

    permissons sets

--- Internal-IT/iam/aws-identity-center/assignments.tf
+++ Internal-IT/iam/aws-identity-center/assignments.tf
@@ -1,10 +1,38 @@
-resource "aws_ssoadmin_account_assignment" "platform_admin_assignment" {
+# Tier 0
+resource "aws_ssoadmin_account_assignment" "tier0_admin" {
+
   instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
   permission_set_arn = aws_ssoadmin_permission_set.platform_admin.arn
 
-  principal_type = "GROUP"
   principal_id   = aws_identitystore_group.groups["Tier0"].group_id
+  principal_type = "GROUP"
 
-  target_type = "AWS_ACCOUNT"
   target_id   = var.management_account_id
+  target_type = "AWS_ACCOUNT"
+}
+
+# Tier 1
+resource "aws_ssoadmin_account_assignment" "tier1_ops" {
+
+  instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
+  permission_set_arn = aws_ssoadmin_permission_set.tier1_ops.arn
+
+  principal_id   = aws_identitystore_group.groups["Tier1"].group_id
+  principal_type = "GROUP"
+
+  target_id   = var.management_account_id
+  target_type = "AWS_ACCOUNT"
+}
+
+# Tier 2
+resource "aws_ssoadmin_account_assignment" "tier2_workload" {
+
+  instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
+  permission_set_arn = aws_ssoadmin_permission_set.tier2_workload.arn
+
+  principal_id   = aws_identitystore_group.groups["Tier2"].group_id
+  principal_type = "GROUP"
+
+  target_id   = var.management_account_id
+  target_type = "AWS_ACCOUNT"
 }
