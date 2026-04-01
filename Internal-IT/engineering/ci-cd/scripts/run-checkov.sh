be7c98c94cb67eda26e4005867e7d99a21291682
Ayush Yadav <official.ayush.066@gmail.com>
2026-04-01 06:44:47 +0530

    checkov script fix

--- Internal-IT/engineering/ci-cd/scripts/run-checkov.sh
+++ Internal-IT/engineering/ci-cd/scripts/run-checkov.sh
@@ -10,8 +10,9 @@ OUTPUT_DIR="$(git rev-parse --show-toplevel)/output"
 mkdir -p "$OUTPUT_DIR"
 
 # Run checkov but DO NOT fail pipeline
-checkov -f output/tfplan.json 
---framework terraform_plan 
--o json > output/checkov-result.json || true
+checkov \
+  -f "$OUTPUT_DIR/tfplan.json" \
+  --framework terraform_plan \
+  -o json > "$OUTPUT_DIR/checkov-result.json" || true
 
 echo "✅ Checkov scan complete"
